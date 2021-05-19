a361a0c7847aa2c6807de7a770924c146aca34ce
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2021-05-19 15:12:12 +0200

    Move test assertions to separate file

---
+++ tests/assert-env-unset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+assert_env_unset() {
+  if [ -n "$(printenv $1)" ]; then
+    echo "Expected secret $1 to be unset"
+    exit 1
+  fi
+}
+
+assert_env_unset "SECRET"
+assert_env_unset "MULTILINE_SECRET"
