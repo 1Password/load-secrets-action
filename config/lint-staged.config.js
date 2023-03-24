fc2eb0c7d5b1bb21f04fa30c4a86acddebb89d22
Dustin Ruetz <dustin.ruetz@agilebits.com>
2023-03-23 20:19:44 -0400

    refactor: remove findrelated test script

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -3,7 +3,7 @@ const lintStagedConfig = {
 	"*.{js,json,md,ts,yaml,yml}": "npm run format:write",
 	"*.{js,ts}": ["npm run lint:fix"],
 	// run testing on all supported file types within the src/ directory
-	"src/**/*.{js,ts}": ["npm run test:findrelated"],
+	"src/**/*.{js,ts}": ["npm run test -- --findRelatedTests"],
 };
 
 export default lintStagedConfig;
