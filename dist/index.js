0eebc66811c3675d2436786f531f3f00d8969883
Dustin Ruetz <dustin.ruetz@agilebits.com>
2023-03-24 13:12:03 -0400

    fix: handle edge case where Error constructor is modified and add comments for context

--- dist/index.js
+++ dist/index.js
@@ -4121,10 +4121,15 @@ const run = async () => {
         await exec.exec(`sh -c "` + parentDir + `/entrypoint.sh"`);
     }
     catch (error) {
+        // It's possible for the Error constructor to be modified to be anything
+        // in JavaScript, so the following code accounts for this possibility.
         // https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
         let message = "Unknown Error";
         if (error instanceof Error) {
             message = error.message;
+        }
+        else {
+            String(error);
         }
         core.setFailed(message);
     }
--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,14 @@ const run = async () => {
 		// Execute bash script
 		await exec.exec(`sh -c "` + parentDir + `/entrypoint.sh"`);
 	} catch (error) {
+		// It's possible for the Error constructor to be modified to be anything
+		// in JavaScript, so the following code accounts for this possibility.
 		// https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
 		let message = "Unknown Error";
 		if (error instanceof Error) {
 			message = error.message;
+		} else {
+			String(error);
 		}
 		core.setFailed(message);
 	}
