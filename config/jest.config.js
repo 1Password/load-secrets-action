27991fd7912a2825e3e6e4cbd8fe68ff32fbf257
Dustin Ruetz <dustin.ruetz@agilebits.com>
2023-03-23 20:23:23 -0400

    fix: move tsconfig.json to root directory, reinstall vercel/ncc for building

--- config/jest.config.js
+++ config/jest.config.js
@@ -13,12 +13,7 @@ const jestConfig = {
 	testEnvironment: "node",
 	testRegex: "(/__tests__/.*|(\\.|/)test)\\.ts",
 	transform: {
-		".ts": [
-			"ts-jest",
-			// required due to custom location of tsconfig.json configuration file
-			// https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig/
-			{ tsconfig: "config/tsconfig.json" },
-		],
+		".ts": ["ts-jest"],
 	},
 	verbose: true,
 };
