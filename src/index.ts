94471ca8fa1ef98afb15eb829a1dc002c3646bf9
Eddy Filip <eddy.filip@agilebits.com>
2023-04-10 10:58:41 +0200

    Make function for executing script

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,13 @@ import * as exec from "@actions/exec";
 
 const run = async () => {
 	try {
-		const currentFile = url.fileURLToPath(import.meta.url);
-		const currentDir = path.dirname(currentFile);
-		const parentDir = path.resolve(currentDir, "..");
 
 		// Get action inputs
 		process.env.INPUT_UNSET_PREVIOUS = core.getInput("unset-previous");
 		process.env.INPUT_EXPORT_ENV = core.getInput("export-env");
 
 		// Execute bash script
-		await exec.exec(`sh -c "` + parentDir + `/entrypoint.sh"`);
+		await executeScript();
 	} catch (error) {
 		// It's possible for the Error constructor to be modified to be anything
 		// in JavaScript, so the following code accounts for this possibility.
@@ -28,5 +25,14 @@ const run = async () => {
 		core.setFailed(message);
 	}
 };
+
+const executeScript = async (): Promise<void> => {
+	const currentFile = url.fileURLToPath(import.meta.url);
+	const currentDir = path.dirname(currentFile);
+	const parentDir = path.resolve(currentDir, "..");
+
+	// Execute bash script
+	await exec.exec(`sh -c "` + parentDir + `/entrypoint.sh"`);
+}
 
 void run();
