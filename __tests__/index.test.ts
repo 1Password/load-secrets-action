3fda38c6c6490dcdee594aa406f5c7d18a63dca3
Lemon <bxb100@outlook.com>
2024-11-28 23:19:13 +0800

    test: remove `require`

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -8,9 +8,8 @@ import * as main from "../src/main";
 const runMock = jest.spyOn(main, "run").mockImplementation();
 
 describe("index", () => {
-  it("calls run when imported", () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require("../src/index");
+  it("calls run when imported", async () => {
+    await import("../src/index");
 
     expect(runMock).toHaveBeenCalled();
   });
